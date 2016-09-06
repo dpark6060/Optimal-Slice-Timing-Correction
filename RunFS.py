5dc86938af903b5c644a6fd4c68724da7e5ddfac
dpark6060 <dpark6060@gmail.com>
2016-09-06 14:06:27 -0400

    Extensive Comments added to script
    No functional changes

--- RunFS.py
+++ RunFS.py
@@ -35,6 +35,8 @@ class WorkingFile:
         self.OrderFile=''
         self.Ref=0
         self.UserOutput=''
+        self.UseTiming=False
+        self.TimingFile=''
         
 
 
@@ -64,7 +66,10 @@ def usage():
     print '\t1: implies ascending slice acquisition from starting slice: (3,5,7,9...)'
     print '\t-1: implies descending slice acquisition from starting slice: (7,5,3,...)\n'
     
-    print('-o:\t Slice Order File.  If present, all interelave parameters are ignored, and slices are shifted using the slice order file')
+    print('-o:\t Slice Order File.  This file is the order in which each slice was acquired.  If present, all interelave parameters are ignored, and slices are shifted using the slice order file')
+    print('\t we refer to the bottom slice in the image as slice 1, not slice 0')
+
+    print('-t:\t Slice Timing File.  If present, all interelave parameters are ignored, and slices are shifted using the slice order file')
     print('\t we refer to the bottom slice in the image as slice 1, not slice 0')
     
     print '-r:\t Set the Reference slice'
@@ -352,18 +357,28 @@ def FiltShift(Inputs):
     return()
 
 def CreateJobsFromFile(f):
-
+    
+    # Sets up jobs for the image based on slices and memory size
+    
     SubmitFiles=[]
     
+    # Initialize the TR and Reference slice    
     Tr=1.0/f.Fs
     RefSlice=0
     
+    # Load Nifti Data
     Nii=nb.load(f.InputNii)
     NiiData=Nii.get_data()
+    
+    # Calculate the third percentile of data - anything below this will be condisered noise/ outside the brain
+    # and will not be processed to save time.  In the final image, any voxels that fall into this category will
+    # be set to zero.
+    
     per=np.percentile(NiiData,3)
     print per
     lx,ly,lz,lt=NiiData.shape
     
+    # If we are using a slice order file
     if f.UseOrder:
         try:
             SliceOrder=np.loadtxt(f.OrderFile)
@@ -374,41 +389,58 @@ def CreateJobsFromFile(f):
         if not len(SliceOrder) ==lz:
             print('Slice Order File does not match number of slices in {}'.format(f.InputNii))
             quit()
-            
+        
+        # Load the file and calculate the exact time of acquision based of the TR and the number of slices
         shift=(TR-TR*1.0/lz)/lz
         SliceShift=SliceOrder*shift
         SliceShift=SliceShift-SliceShift[Start]
     
+    
+    #TODO Add Slice Timing File Here
+    
+    # Otherwise just create a slice timing file using the start slice and intereleave parameter
     else:
         
         SliceOrder, SliceShift = MakeT(f.Int, f.Ref, lz, Tr,f.startSlice, f.Direction)
     
     print('Using Slice Order:\n{}'.format(SliceOrder+1))
     
-    GBperCore=f.mem/f.Cores*0.9
-    BytePerCore=GBperCore*np.power(1024,3)    
-    Nelements=lt*2*(f.Fnew/f.Fs)
-    MemTotal=Nelements*31.48
+    
+    # Calculate the size that each job can be given the availible memory and number of cores.
+    GBperCore=f.mem/f.Cores*0.9 # Availible memory (GB) divided by the number of cores - reduced by 0.9 - a conservitive measure on how much memory each job can have
+    BytePerCore=GBperCore*np.power(1024,3)  # Calculate the number of bytes this represents per job
+    Nelements=lt*2*(f.Fnew/f.Fs)    # Calculate the number of elements there will be in one voxel's time series after upsampling and padding
+    MemTotal=Nelements*31.48        # 31.48 is a calculated value for how many bytes a single array element takes up in numpy - an estimate of how much memory a single voxel's timeseries will take up
 
     
     
-    
-    while MemTotal>BytePerCore:
+    # If the amount of memory required for ONE voxel is more than we can spare given the number of cores we're using, reduce the number of cores by one.
+    while MemTotal>BytePerCore: 
         f.Cores=f.Cores-1
         if f.Cores<1:
             print ('Need More memory, there WILL (probably) be a memory error.  use -Force to force')
+            print ('THIS IS NOT RECOMMENDED, THIS WILL CRASH YOUR COMPUTER')
+            
+            # If for some reason you know better than my little program, force the computer to try to process it anyways.
             if f.force==False:
                 quit()
-            
+         
+        # Recalculate the bytes per core with the new core count   
         GBperCore=f.mem/f.Cores*.9
         BytePerElement=np.dtype(float).itemsize
         BytePerCore=GBperCore*np.power(1024,3)
         
     
+    # Now determine just HOW many voxels we can send to each core at a time without going over our alloted memory
+    # The bytes per core divided by the total memory required for one voxel, rounded down
     txtLen=int(np.floor(BytePerCore)/(MemTotal))
+    
+    # Saved in a parameters file that is referenced later if you try to rerun the analysis - if nothing in the parameters file has changed, the analysis does not re-run.
     ParameterFile=os.path.join(f.TxtOutput,'Parameters.txt')
     Rerun=True
     
+    
+    # If the parameters file exists already, load it and read some values
     if os.path.exists(ParameterFile):
         parameters=open(ParameterFile,'r')
         line=parameters.read()
@@ -416,50 +448,65 @@ def CreateJobsFromFile(f):
         OldtxtLen=int(groups.groups()[0])
         groups=re.search('Nelements:(.*)\n',line)
         OldNelements=int(np.round(float(groups.groups()[0])))
+        
+        # If we're still using the same number of voxels and elemets pre voxel, nothing has changed...
         if (OldtxtLen==txtLen and OldNelements==Nelements) and f.force==False:
+            #...So do not rerurn.
             Rerun=False
  
     SliceLen=glob.glob(os.path.join(f.TxtOutput,'SliceLen_*.txt'))
     
-    
+    # If there are no previous sliceLen files (glob returns empty), or we're forcing a rerun
     if (not SliceLen) or Rerun:
         
-        
+        # Remove all previous logs if we're rerunning
         if Rerun:
             cmd='rm {}/*.txt'.format(f.TxtOutput)
             pr=sp.Popen(cmd,shell=True)
             pr.wait()
-            
+        
+        #Extract NII data slice by slice and reshape into a 2-D array
         for z in range(lz):
             Extract=np.squeeze(NiiData[:,:,z,:])
             Extract=np.reshape(Extract,(-1,lt))
             LexOrig=len(Extract)
+            
+            # Any time series where the min along the time axis is still above the bottom 3% intensity value of the data, consider this signal
             ind=np.where(Extract.min(1)>per)
+            # Extract those signals
             Extract=Extract[ind]
+            
+            # Create two text files, one that stores the length of the slice (number of voxels) before removing noise voxels, and another that stores the xyz coordinates of these voxels.
             lentxt=os.path.join(f.TxtOutput,'SliceLen_{}.txt'.format(z))
             indtxt=os.path.join(f.TxtOutput,'SliceInd_{}.txt'.format(z))
             
+            # Save the values
             np.savetxt(lentxt,[LexOrig])
             np.savetxt(indtxt,ind[0])
-              
+            
+            # Extract voxel time series' and save as text files for parallelization - remember, can only store txtLen voxels' worth of time series.
             for ii,inc in enumerate(range(0,len(Extract),txtLen)):
                 
+                # the current index plus the increment is larger than the file, just take to the end.
                 if inc+txtLen>=len(Extract):
                     txtdat=Extract[inc:]
                 else:
                     txtdat=Extract[inc:inc+txtLen]
                 
-                TxtName=os.path.join(f.TxtOutput,'Image_{}_{}.txt'.format(z,ii))
-                
+                # Save the voxel time series as a text file indicating the slice number and the portion of that slice
+                TxtName=os.path.join(f.TxtOutput,'Image_{}_{}.txt'.format(z,ii))                
                 np.savetxt(TxtName,txtdat)
-            
+                
+                # Create a job file containing the timing shift of the slice that this job is working on, the image file it's working on, and the output directory.
                 SubmitFiles.append([TxtName,'{}'.format(SliceShift[z]),'{}'.format(f.Fs),'{}'.format(f.Fnew),os.path.join(f.StdOutput,'Image_{}_{}.txt'.format(z,ii)),os.path.join(f.ErrorOutput,'Image_{}_{}.txt'.format(z,ii))])
         
+        # Write more values to the parameter file
         ParamOut=open(ParameterFile,'w')
         ParamOut.write('txtLen:{}\n'.format(txtLen))
         ParamOut.write('Nelements:{}\n'.format(Nelements))
         ParamOut.close()
-           
+    
+    # But if the text files exist already, simply recreate the jobs.      
     else:
         print('{} Text Files Exist, Recreating Jobs'.format(f.FileBase))
         
@@ -478,66 +525,62 @@ def CreateJobsFromFile(f):
                 if not os.path.exists(Output):
                 
                     SubmitFiles.append([TxtName,'{}'.format(SliceShift[z]),'{}'.format(f.Fs),'{}'.format(f.Fnew),os.path.join(f.StdOutput,'Image_{}_{}.txt'.format(z,ii)),os.path.join(f.ErrorOutput,'Image_{}_{}.txt'.format(z,ii))])
-
+    
+    # Save the slice order and slice shift files for reference.
     np.savetxt(os.path.join(f.TxtOutput,'SliceOrder.txt'),SliceOrder)
     np.savetxt(os.path.join(f.TxtOutput,'SliceShift.txt'), SliceShift)
+    
+    # Return a list of jobs to submit.
     return(SubmitFiles)
 
 def SetUpWorkingPaths(f):
+    # This creates a working directory structured for the code to work on.  
     InputNii=f.InputNii
     
+    # If the user didn't specify an output directory, simply work in the path of the input NII file
     if f.UserOutput=='':    
         RootDir,FileName=os.path.split(InputNii)
-        
+    
+    # Otherwise create and set up a working directory where they user specified
     else:
         RootDir,FileName=os.path.split(f.UserOutput)
         if RootDir=='':
             RootDir,trash=os.path.split(InputNii)
 
-
+    # Strip the file name of its extension
     trash='.exe'
     FileBase=FileName
     while trash:
         FileBase,trash=os.path.splitext(FileBase)
     
-    
-    
-    
-    
+    # Save the final output in the specified output directory with the suffix "_FS"
     OutputFile=os.path.join(RootDir,'{}_FS.nii.gz'.format(FileBase))
     
-    
-    
+    # If that output exists, I guess you're rerunning, so let's just add a number to the end so it doesn't save over
     n=1
-    
     while os.path.exists(OutputFile):
         OutputFile=os.path.join(RootDir,'{}_FS{}.nii.gz'.format(FileBase,n))
         n=n+1
     
-    WrkDir=os.path.join(RootDir,'{}_Work'.format(FileBase))
-        
+    # Create the work directory and sub-directory structure
+    WrkDir=os.path.join(RootDir,'{}_Work'.format(FileBase))        
     TxtOutput=os.path.join(WrkDir,'WorkFiles')
     JobDir=os.path.join(WrkDir,'ShFiles')
     ErrorOutput=os.path.join(WrkDir,'StdError')
-    StdOutput=os.path.join(WrkDir,'StdOutput')
-    
+    StdOutput=os.path.join(WrkDir,'StdOutput')   
     
     if not os.path.exists(WrkDir):
-        os.makedirs(WrkDir)
-    
+        os.makedirs(WrkDir)    
     if not os.path.exists(TxtOutput):
-        os.makedirs(TxtOutput)
-    
+        os.makedirs(TxtOutput)    
     if not os.path.exists(JobDir):
-        os.makedirs(JobDir)
-        
+        os.makedirs(JobDir)        
     if not os.path.exists(ErrorOutput):
-        os.makedirs(ErrorOutput)
-    
+        os.makedirs(ErrorOutput)    
     if not os.path.exists(StdOutput):
         os.makedirs(StdOutput)
         
-    
+    # Set internal variables
     f.FileBase=FileBase
     f.FileName=FileName
     f.OutputFile=OutputFile
@@ -550,50 +593,68 @@ def SetUpWorkingPaths(f):
     f.RootDir=RootDir
     return(f)
 
-def CompileNii(f):
-    OriginalFile=f.InputNii
+def CompileNii(f):    
+    # Reconstruct the processed Time series text files into a NIFTI file
     
+    # First load the original file for dimension information
+    ## TODO This could be stored in the f structure maybe
+    OriginalFile=f.InputNii    
     UnCorNii=os.path.join(OriginalFile)
     UnCorNii=nb.load(UnCorNii)
     lx,ly,lz,lt=UnCorNii.get_data().shape
     
+    # Load directory information for output file structure
     BaseDir=f.RootDir
     fname=f.FileBase
-    subID=fname
-
+    subID=fname    
     SplitBase=subID
-    
-    StackList=np.zeros((lx,ly,1,lt))
-    
     txtDir=f.TxtOutput
     
+    # Reconstruction will occur by slice, each slice being this size and shape.
+    StackList=np.zeros((lx,ly,1,lt))    
+    
+    # Loop through and reconstruct each slice
     for z in range(lz):
+        
+        #The Original time series matrix was lx*ly by lt in size, so create a variable to store that
         zstack=np.zeros((lx*ly,lt))
+        
+        # Find all the text files that belong to this slice
         ztxts=os.path.join(txtDir,'FS_{}_{}_*.txt'.format('Image',z))
         nzchunks=len(glob.glob(ztxts))
+        
+        # Load the SliceInd files, which tell us which index each row in the FS text file corresponds to in the Original time series file
         ind=np.loadtxt(os.path.join(txtDir,'SliceInd_{}.txt'.format(z)))
         ind=np.array([int(n) for n in ind])
         
+        # The voxels we filtered aren't every voxel in the slice, so we'll store them in a separate variable that will be less than or equal to lx*ly by lt in size
         zchunks=[]
         
-        
+        # Now loop through the text files associated with the slice
         for ii in range(nzchunks):
             #print(os.path.join(txtDir,'FS_{}_{}_{}.txt'.format('Image',z,ii)))
             newdata=np.loadtxt(os.path.join(txtDir,'FS_{}_{}_{}.txt'.format('Image',z,ii)))
             
+            # Resize the loaded data so it can be appended to a 2d matrix
             if newdata.ndim==1:
                 newdata=np.expand_dims(newdata,0)
-                
-            zchunks.extend(newdata)
             
+            # Add the new data do the zchunks 
+            zchunks.extend(newdata)
+        
+        # Assuming that there were ANY timeseries text files for this slice, just assign the "zchunks" rows to the "zstack" rows as indicated by "ind"
         if nzchunks>0:
             zstack[ind,:]=zchunks
-            
-        zstack=np.reshape(zstack,(lx,ly,1,lt))
-        StackList=np.concatenate((StackList,zstack),2)
         
+        # Reshape the processed time series back to the proper slice dimensions
+        zstack=np.reshape(zstack,(lx,ly,1,lt))
+        # Add this slice to the stack of slices
+        StackList=np.concatenate((StackList,zstack),2)
+    
+    # Apparently my code attaches an extra slice of zeros, so just crop it off.
     StackList=StackList[:,:,1:,:]
     
+    # Save the image using the affine and header from the original Nifti file
     img=nb.Nifti1Image(StackList,UnCorNii.get_affine(),UnCorNii.get_header())
     savefile=os.path.join(BaseDir,'{}_FS'.format(subID))
     nb.loadsave.save(img,savefile)
@@ -601,25 +662,34 @@ def CompileNii(f):
 
 def main(WorkingFile):
     
+    # If the user didn't set the cores (not recommended) we have to guess - take the core count and as long as you have more than two, take one fewer than the max number of cores.
     if WorkingFile.Cores==0:
         WorkingFile.Cores=multiprocessing.cpu_count()
         if WorkingFile.Cores>2:
             WorkingFile.Cores=WorkingFile.Cores-1
-
+    
+    # Set up a working directory based on user specifications (or lack there of)
     WorkingFile = SetUpWorkingPaths(WorkingFile)
     
+    # Get the dimensions of the Nifti Image and Calculate the new frequency needed to sample the image
     x,y,z,t=nb.load(WorkingFile.InputNii).get_shape()
     Fnew=np.ceil(z*WorkingFile.Fs)    
     WorkingFile.Fnew=Fnew
     
+    # If there's already an output file, we're quitting the program.  It takes too long to run.
     if os.path.exists(WorkingFile.OutputFile):
         print('FS arleady run on this file:\nOutput Located at:\n{}'.format(WorkingFile.OutputFile))
         quit()
     
+    # Create jobs to submit to parallel processing
     WorkingFile.SubmitFiles=CreateJobsFromFile(WorkingFile)
-
+    
+    # This is so that if you error out mid-image, you won't need to rerun the ENTIRE thing, it's smart enough to see that there are slices that are already processed
+    # Get a count of the number of text files that need to be be processed
     NtxtFiles=len(glob.glob(os.path.join(WorkingFile.TxtOutput,'Image*.txt')))
+    # And a count of the number of files that ARE processed
     ProcessedFiles=len(glob.glob(os.path.join(WorkingFile.TxtOutput,'FS_Image*.txt')))
+    # If they're equal, we're done...if not, create a parallel pool of the jobs created
     if not ProcessedFiles==NtxtFiles:
         p=Pool(WorkingFile.Cores)
         p.map(FiltShift,WorkingFile.SubmitFiles)
@@ -628,21 +698,26 @@ def main(WorkingFile):
     else:
         print 'Completed for {}'.format(WorkingFile.FileBase)
     
+    # Check for Completion condition - while the processed files are fewer than the original files, keep waiting for the jobs to run
     ProcessedFiles=len(glob.glob(os.path.join(WorkingFile.TxtOutput,'FS_{}*.txt'.format('Image'))))
-    
     while ProcessedFiles<NtxtFiles:
         print('Waiting for Files to Finish')
         ProcessedFiles=len(glob.glob(os.path.join(WorkingFile.TxtOutput,'FS_{}*.txt'.format('Image'))))
         time.sleep(60)
-        
+    
+    # Once the jobs are completed, Reconstruct the text files into a Nifti Image
     CompileNii(WorkingFile)
     
+    # Gzip that if it isn't already
     cmd='gzip -f {}/{}_FS.nii'.format(WorkingFile.RootDir,WorkingFile.FileBase)
     pr=sp.Popen(cmd,shell=True)
     pr.wait()
+    
+    # That's it, we're done.  Go Home.
 
 if __name__ == '__main__':
     
+    # I'm not commenting this, it's just a crude parser.
     #-in <InputFile> -tr <TR> -i <Int> [-s <StartSlice>] [-d <Direction>] [-o <SliceORderFile>] [-r <RefSlice>] [-c <nCores>] [-m <Memory>] [-Force]'
     if len(sys.argv)>=3:
 
